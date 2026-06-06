9b0fc7d58f4664f04b4333ac888e72501649eb8a
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-06-06 11:07:49 +0200

    fix sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,16 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
     namespace = "com.example.diceroller"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.diceroller"
         minSdk = 29
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
