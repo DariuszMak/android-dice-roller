3e66394263b4a5df8900d2d305c031c9dcce36f1
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-06-06 11:31:27 +0200

    Revert "fix sdk version"
    This reverts commit 9b0fc7d58f4664f04b4333ac888e72501649eb8a.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,15 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
 }
 
 android {
     namespace = "com.example.diceroller"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.diceroller"
         minSdk = 29
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
